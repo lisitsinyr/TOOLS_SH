ae5858256214eb738ccab5e0938f649fdb681a98
lisitsinyr <lisitsinyr@gmail.com>
2023-12-28 14:46:07 +0400

    Git Bash commit update

---
+++ SH/git_update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -------------------------------------------------------------------
+# git_update.sh
+# -------------------------------------------------------------------
+
+#:begin
+git add --all
+git commit -m "Git Bash commit update"
+
+git push -u origin main
+
+#:Exit
