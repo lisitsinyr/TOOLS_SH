b0ab3e141760a69255cb07bf10aff74b1d7276a9
lisitsinyr <lisitsinyr@gmail.com>
2024-01-04 11:17:39 +0400

    Git Bash commit update

--- lyrgit_push_main.bat
+++ lyrgit_push_main.bat
@@ -5,6 +5,10 @@ rem -------------------------------------------------------------------
 chcp 1251
 
 :begin
+git add --all
+
+git commit -m "Git Bash commit update"
+
 git push -u origin main
 
 :Exit
