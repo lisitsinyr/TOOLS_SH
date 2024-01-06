d1adbfe7cb9abdfd752bf881a623977f7336e486
lisitsinyr <lisitsinyr@gmail.com>
2024-01-06 17:31:15 +0400

    Git Bash commit update

--- lyrgit_remote_add_origin.bat
+++ lyrgit_remote_add_origin.bat
@@ -1,10 +1,8 @@
 @echo off
 rem -------------------------------------------------------------------
-rem lyrgit_remote_v.bat
+rem lyrgit_remote_add_origin.bat
 rem ----------------------------------------------------------------------------
-rem 6.Сотрудничать [***]
-rem ----------------------------------------------------------------------------
-rem Верифицируем что удаленный репозитарий связан с нашим
+rem 
 rem ----------------------------------------------------------------------------
 rem usage: git remote [-v | --verbose]
 rem    or: git remote add [-t <branch>] [-m <master>] [-f] [--tags | --no-tags] [--mirror=<fetch|push>] <name> <url>
@@ -20,11 +18,26 @@ rem    or: git remote set-url [--push] <name> <newurl> [<oldurl>]
 rem    or: git remote set-url --add <name> <newurl>
 rem    or: git remote set-url --delete <name> <url>
 rem 
-rem     -v, --verbose         be verbose; must be placed before a subcommand
-rem -------------------------------------------------------------------
+rem     -v, --[no-]verbose    be verbose; must be placed before a subcommand
+rem ----------------------------------------------------------------------------
 chcp 1251
 
-:begin
-git remote -v
+rem -------------------------------------------------------------------
+:P1
+echo Check 1 parametr
+if "%1" == "" goto P1_Input
+set /p GlobalRepository="%1"
+goto Begin1
+:P1_Input
+set /p GlobalRepository=GlobalRepository:
+if "%GlobalRepository%" == "" goto P1_Error
+goto Begin1
+:P1_Error
+echo Parametr GlobalRepository not set
+rem -------------------------------------------------------------------
+
+:begin1
+echo %GlobalRepository%
+git remote add origin %GlobalRepository%
 
 :Exit
