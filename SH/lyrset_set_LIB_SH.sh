6c0137bb7b3b9e10a5cdf7e228a3445672f7d5c9
lisitsinyr <lisitsinyr@gmail.com>
2024-02-05 16:35:12 +0400

    Git Bash commit update

--- SH/lyrset_set_LIB_SH.sh
+++ SH/lyrset_set_LIB_SH.sh
@@ -5,9 +5,24 @@
 
 #:begin
 echo '---------------------------------------------'
-echo ' set_LIB_SH '
+echo 'set_LIB_SH'
 echo '---------------------------------------------'
-LIB_SH=""
+
+case "$UNAME" in
+    'ASUS-W10P')
+        LIB_SH='/d/PROJECTS_LYR/CHECK_LIST/01_OS/03_UNIX/TOOLS_SH'
+        ;;
+    'ASUS-U2204-VB')
+        LIB_SH='/home/lyr/PROJECTS_LYR/CHECK_LIST/01_OS/03_UNIX/TOOLS_SH'
+        ;;
+    *)
+        echo "Компьютер не определен...!"
+        exit 1
+        ;;
+esac
+
+
+echo "$LIB_SH"
 
 #:Exit
 
