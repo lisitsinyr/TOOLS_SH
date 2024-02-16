fa02c3aab551fc26c3bb43fdc4f0be7222ed25aa
lisitsinyr <lisitsinyr@gmail.com>
2024-02-16 10:25:11 +0400

    20240216102510

---
+++ LIB/[lyrxxx_]PATTERN.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+# -----------------------------------------------
+# [lyrxxx_]PATTERN.sh
+# -----------------------------------------------
+
+# -------------------------------------------------------------------
+# Каталог журнала: каталог
+#     LOG_DIR=
+# Файл журнала: имя+расширение
+#     LOG_BASEFILENAME=
+# Файл журнала: имя
+#     LOG_FILENAME=
+# Формат имени файлф журнала: FILENAME,DT,...
+#     LOG_FILENAME_FORMAT=
+# Параметры журнала
+#     LOG_OPT=11
+# -------------------------------------------------------------------
+
+#====================================================================
+PROJECTS='PROJECTS_UNIX'
+# -------------------------------------------------------------------
+CURRENT_SYSTEM=$(uname -a)
+UNAME=$(uname -n)
+USERNAME=$(whoami)
+# -------------------------------------------------------------------
+# БИБЛИОТЕКА LYR
+# -------------------------------------------------------------------
+UNAME=$(uname -n)
+if [[ -z "$LIB_SH" ]] ; then
+    case "$UNAME" in
+        'ASUS-W10P')
+            PROJECTS_LYR_DIR='/d/PROJECTS_LYR'
+            ;;
+        'ASUS-U2204-VB' | 'ASUS-U2204-VM' | 'ASUS-U2310-VB' | 'ASUS-U2310-VB' | 'ASUS-U2310')
+            PROJECTS_LYR_DIR='/home/lyr/PROJECTS_LYR'
+            ;;
+        *)
+            echo "Компьютер не определен...!"
+            exit 1
+            ;;
+    esac
+    PROJECTS_DIR="$PROJECTS_LYR_DIR/CHECK_LIST/01_OS/03_UNIX/$PROJECTS"
+    echo PROJECTS_DIR: $PROJECTS_DIR
+    LIB_SH="$PROJECTS_LYR_DIR/CHECK_LIST/01_OS/03_UNIX/$PROJECTS/TOOLS_SH/LIB"
+    echo LIB_SH: $LIB_SH
+fi
+echo Каталог проектов LYR: $PROJECTS_DIR
+echo Каталог библиотеки LYR: $LIB_SH
+if [[ ! -d "$LIB_SH" ]] ; then
+    echo Каталог библиотеки LYR $LIB_SH не существует...
+    exit 1
+fi
+# -------------------------------------------------------------------
+# запуск скриптов БИБЛИОТЕКИ LYR
+# -------------------------------------------------------------------
+source "$LIB_SH/__SET_LIB.sh"
+source "$LIB_SH/LYRFileUtils.sh"
+source "$LIB_SH/LYRLog.sh"
+source "$LIB_SH/LYRConst.sh"
+source "$LIB_SH/LYRDateTime.sh"
+source "$LIB_SH/LYRDateTime.txt"
+source "$LIB_SH/LYRSupport.sh"
+#====================================================================
+
+echo ==================================================================
+echo SET %0 ...
+echo ==================================================================
+# CURRENT_DIR - Текущий каталог
+CURRENT_DIR=$(pwd)
+echo CURRENT_DIR: $CURRENT_DIR
+DIR_SAVE=$CURRENT_DIR
+echo DIR_SAVE: $DIR_SAVE
+# SH_DIR=D:\TOOLS\TOOLS_BAT
+# __SET_BAT_DIR
+# __SET_VAR_SCRIPT %0
+# __SET_VAR_DEFAULT
+# __SET_VAR_PROJECTS
+# __SET_CHECK_REPO
+# __SET_LOG
+# __START_LOG
+
+
+# -------------------------------------------------------------------
+# StartLogFile
+# -------------------------------------------------------------------
+# StartLogFile "$0" "" ""
+# -------------------------------------------------------------------
+
+#:begin
+#=================================================
+# AddLog $loAll $NOTSET 'NOTSET'
+# AddLog $loAll $DEBUG 'DEBUG'
+# AddLog $loAll $INFO 'INFO'
+# AddLog $loAll $WARNING 'WARNING'
+# AddLog $loAll $ERROR 'ERROR'
+# AddLog $loAll $CRITICAL 'CRITICAL'
+# AddLog $loAll $DEBUGTEXT 'DEBUGTEXT'
+# AddLog $loAll $BEGIN 'BEGIN'
+# AddLog $loAll $END 'END'
+# AddLog $loAll $PROCESS 'PROCESS'
+# AddLog $loAll $TEXT 'TEXT'
+#=================================================
+
+#=================================================
+# AddLog $loAll $TEXT '--------------------------------------'
+# str=''
+# str=""
+# result=$('')
+# AddLog $loAll $TEXT 'str:'"$str"
+# AddLog $loAll $TEXT 'result:'"$result"
+# AddLog $loAll $TEXT '--------------------------------------'
+#=================================================
+#:end
+
+# -------------------------------------------------------------------
+# StopLogFile
+# -------------------------------------------------------------------
+# StopLogFile
+# -------------------------------------------------------------------
+
+exit 0
+
+#:Exit
---
+++ LIB/__SET_LIB.sh
@@ -0,0 +1,263 @@
+#!/bin/bash
+# -----------------------------------------------
+# LYRConst.sh
+# -----------------------------------------------
+# -------------------------------------------------------------------
+# ФУНКЦИИ
+#     function __SET_VAR_SCRIPT
+#     function __SET_BAT_DIR
+#     function __SET_VAR_DEFAULT
+#     function __SET_VAR_PROJECTS
+#     function __SET_CHECK_REPO
+#     function __SET_LOG
+#     function __START_LOG
+#     function __STOP_LOG
+# -------------------------------------------------------------------
+
+# =================================================
+# ФУНКЦИИ
+# =================================================
+
+# --------------------------------------------------------------------------------
+# function __SET_VAR_SCRIPT [%1]
+# --------------------------------------------------------------------------------
+function __SET_VAR_SCRIPT { #
+# beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __SET_VAR_SCRIPT ...
+    # echo ---------------------------------------------------------------
+    # Файл скрипта: каталог+имя+расширение
+    set SCRIPT_FULLFILENAME=%~f1
+    # echo SCRIPT_FULLFILENAME: %SCRIPT_FULLFILENAME%
+    # Файл скрипта: имя+расширение
+    set SCRIPT_BASEFILENAME=%~n1%~x1
+    # echo SCRIPT_BASEFILENAME: %SCRIPT_BASEFILENAME%
+    # Файл скрипта: имя
+    set SCRIPT_FILENAME=%~n1
+    # echo SCRIPT_FILENAME: %SCRIPT_FILENAME%
+}
+# endfunction
+
+# --------------------------------------------------------------------------------
+# function __SET_BAT_DIR
+# --------------------------------------------------------------------------------
+function __SET_BAT_DIR { #
+# beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __SET_BAT_DIR
+    # echo ---------------------------------------------------------------
+    # Каталог BAT_DIR: каталог
+    if "%BAT_DIR%" == "" (
+        set BAT_DIR=D:\TOOLS\TOOLS_BAT
+        set BAT_DIR=D:\PROJECTS_LYR\CHECK_LIST\03_SCRIPT\04_BAT\TOOLS_BAT
+    )
+    # echo BAT_DIR: %BAT_DIR%
+}
+# endfunction
+
+# --------------------------------------------------------------------------------
+# function __SET_VAR_DEFAULT
+# --------------------------------------------------------------------------------
+function __SET_VAR_DEFAULT { #
+# beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __SET_VAR_DEFAULT
+    # echo ---------------------------------------------------------------
+    # echo -------------------------------------------------------
+    # echo 1.Переменные по умолчанию
+    # echo    %LOG_DIR%
+    # echo    %LOG_FILENAME%
+    # echo    %DATETIME_STAMP%
+    # echo    %LOG_FILENAME_FORMAT%
+    # echo    %LOG_OPT%
+    # echo -------------------------------------------------------
+    # LOG_FILENAME - Файл журнала [имя]
+    set LOG_FILENAME=%REPO_NAME%_xxxxxxxxxxxxxxxxxx
+    set LOG_FILENAME=
+    # echo LOG_FILENAME: %LOG_FILENAME%
+    # -------------------------------------------------------------------
+    # DATETIME_STAMP - формат имени файла журнала [YYYYMMDDHHMMSS]
+    set DATETIME_STAMP=%date:~6,4%%date:~3,2%%date:~0,2%%TIME:~0,2%%TIME:~3,2%%TIME:~6,2%
+    # echo DATETIME_STAMP [YYYYMMDDHHMMSS]: %DATETIME_STAMP%
+    # -------------------------------------------------------------------
+    # LOG_FILENAME_FORMAT - Формат имени файла журнала [FILENAME,DT,...]
+    if "%LOG_FILENAME_FORMAT%"=="" (
+    set LOG_FILENAME_FORMAT=FILENAME
+    # set LOG_FILENAME_FORMAT=DATETIME
+    )
+    # echo LOG_FILENAME_FORMAT [FILENAME,DT,...]: %LOG_FILENAME_FORMAT%
+    # -------------------------------------------------------------------
+    # LOG_OPT - Параметры журнала [11]
+    if "%LOG_OPT%"=="" (
+        set LOG_OPT=11
+    )
+    # echo LOG_OPT [11]: %LOG_OPT%
+}
+# endfunction
+
+# --------------------------------------------------------------------------------
+# function __SET_VAR_PROJECTS
+# --------------------------------------------------------------------------------
+function __SET_VAR_PROJECTS { #
+# beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __SET_VAR_PROJECTS
+    # echo ---------------------------------------------------------------
+    # echo -------------------------------------------------------
+    # echo 2.Проект [PROJECTS]
+    # echo    %PROJECTS%
+    # echo    %UNAME%
+    # echo    %USERNAME%
+    # echo    %PROJECTS_LYR_DIR%
+    # echo    %PROJECTS_DIR%
+    # echo -------------------------------------------------------
+    set PROJECTS=PROJECTS_BAT
+    # echo PROJECTS: %PROJECTS%
+    # -------------------------------------------------------------------
+    set CURRENT_SYSTEM=%OS%
+    # echo CURRENT_SYSTEM: %CURRENT_SYSTEM%
+    # -------------------------------------------------------------------
+    set UNAME=%COMPUTERNAME%
+    # echo UNAME: %UNAME%
+    # -------------------------------------------------------------------
+    set USERNAME=%USERNAME%
+    # echo USERNAME: %USERNAME%
+    # -------------------------------------------------------------------
+    set PROJECTS_LYR_DIR=D:\PROJECTS_LYR
+    # echo PROJECTS_LYR_DIR: %PROJECTS_LYR_DIR%
+    # -------------------------------------------------------------------
+    set PROJECTS_DIR=%PROJECTS_LYR_DIR%\CHECK_LIST\03_SCRIPT\04_BAT\%PROJECTS%
+    # echo PROJECTS_DIR: %PROJECTS_DIR%
+}
+# endfunction
+
+# --------------------------------------------------------------------------------
+# function __SET_CHECK_REPO { #
+# --------------------------------------------------------------------------------
+function __SET_CHECK_REPO
+# beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __SET_CHECK_REPO
+    # echo ---------------------------------------------------------------
+    # echo -------------------------------------------------------
+    # echo 3.Наличие репозитария
+    # echo    %REPO_NAME%
+    # echo -------------------------------------------------------
+    # REPO_NAME - Имя репозитария
+    set REPO_NAME=
+    # -------------------------------------------------------------------
+    # REPO_INI - Файл с параметрами репозитария
+    set REPO_INI=REPO.ini
+    # echo REPO_INI [REPO.ini]: %REPO_INI%
+    # -------------------------------------------------------------------
+    # Проверка существования файла REPO.ini
+    if not exist %REPO_INI% (
+        echo INFO: File %REPO_INI% not exist
+        # exit /b 1
+    ) else (
+        for /f "eol=# delims== tokens=1,2" %%i in (%REPO_INI%) do (
+            # В переменной i - ключ
+            # В переменной j - значение
+            set %%i=%%j
+            # echo %%i: %%%j%
+        )
+    )
+    # echo REPO_NAME: %REPO_NAME%
+}
+# endfunction
+
+# --------------------------------------------------------------------------------
+# function __SET_LOG
+# --------------------------------------------------------------------------------
+function __SET_LOG { #
+# beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __SET_LOG
+    # echo ---------------------------------------------------------------
+    # echo -------------------------------------------------------
+    # echo 4.Настройка Каталога журналов и имени журнала 
+    # echo    %LOG_DIR%
+    # echo    %LOG_FILENAME%
+    # echo    %LOG_OPT1%
+    # echo    %LOG_OPT2%
+    # echo -------------------------------------------------------
+
+    #if "%__SET_LOG__%"=="1" (echo __SET_LOG__: %__SET_LOG__% && exit /b 0) else (set __SET_LOG__=1)
+
+    # LOG_DIR - Каталог журнала [каталог]
+    #if "%LOG_DIR%"=="" (
+    #    set LOG_DIR=%PROJECTS_LYR_DIR%\LOGS
+    #)
+    # echo LOG_DIR: %LOG_DIR%
+    #if not exist %LOG_DIR% (
+    #    echo ERROR: Dir %LOG_DIR% not exist
+    #    exit /b 1
+    #)
+    # LOG_FILENAME - Файл журнала [имя]
+    #if "%LOG_FILENAME%"=="" (
+    #    if "%LOG_FILENAME_FORMAT%"=="FILENAME" (
+    #        set LOG_FILENAME=%SCRIPT_FILENAME%
+    #    ) else (
+    #        if "%LOG_FILENAME_FORMAT%"=="DATETIME" (
+    #            set LOG_FILENAME=%DATETIME_STAMP%
+    #        ) else (
+    #            echo ERROR: LOG_FILENAME not set 
+    #            exit /b 1
+    #        )
+    #    )
+    #)
+    # echo LOG_FILENAME: %LOG_FILENAME%
+    # -------------------------------------------------------------------
+    # LOG_FULLFILENAME - Файл журнала [каталог+имя+расширение]
+    #if "%REPO_NAME%"=="" (
+    #    set LOG_FULLFILENAME=%LOG_DIR%\%LOG_FILENAME%.log
+    #) else (
+    #    set LOG_FULLFILENAME=%LOG_DIR%\%REPO_NAME%_%LOG_FILENAME%.log
+    #)
+    # echo LOG_FULLFILENAME: %LOG_FULLFILENAME%
+    # -------------------------------------------------------------------
+    # LOG_OPT1 - Параметр журнала [1]
+    #set LOG_OPT1=%LOG_OPT:~0,1%
+    #if "%LOG_OPT1%"=="" (
+    #    set LOG_OPT1=1
+    #)
+    # echo LOG_OPT1 [1]: %LOG_OPT1%
+    # -------------------------------------------------------------------
+    # LOG_OPT2 - Параметры журнала [1]
+    #set LOG_OPT2=%LOG_OPT:~1,1%
+    #if "%LOG_OPT2%"=="" (
+    #    set LOG_OPT2=1
+    #)
+    # echo LOG_OPT2 [1]: %LOG_OPT2%
+}
+# endfunction
+
+# --------------------------------------------------------------------------------
+# function __START_LOG
+# --------------------------------------------------------------------------------
+function __START_LOG { #
+    # beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __START_LOG
+    # echo ---------------------------------------------------------------
+    #if "%__START_LOG__%"=="1" (echo __START_LOG__: %__START_LOG__% && exit /b 0) else (set __START_LOG__=1)
+    #echo ================================================================= > %LOG_FULLFILENAME%
+    #echo LOG_FULLFILENAME: %LOG_FULLFILENAME%                              >> %LOG_FULLFILENAME%
+    #echo ================================================================= >> %LOG_FULLFILENAME%
+}
+# endfunction
+
+# --------------------------------------------------------------------------------
+# :function __STOP_LOG
+# --------------------------------------------------------------------------------
+function __STOP_LOG { #
+# beginfunction
+    # echo ---------------------------------------------------------------
+    # echo __STOP_LOG
+    # echo ---------------------------------------------------------------
+}
+# endfunction
+
+# ===================================================================
+
+#:Exit
