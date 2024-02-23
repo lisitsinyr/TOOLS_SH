899639fe6f4e1e08aa1e5d2331be728e9564b52b
lisitsinyr <lisitsinyr@gmail.com>
2024-02-23 17:19:37 +0400

    20240223171937

---
+++ SH/lyrspm_install_grub.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+# -------------------------------------------------------------------
+# lyrspm_install_grub.sh
+# -------------------------------------------------------------------
+
+#:begin
+echo '---------------------------------------------'
+echo ' install_grub '
+echo '---------------------------------------------'
+sudo add-apt-repository ppa:danielrichter2007/grub-customizer
+
+sudo apt-get update
+
+sudo apt install grub-customizer
+
+#:Exit
+
