ae2e791d7729552ff3e6d38e0f747ce4cf40879f
lisitsinyr <lisitsinyr@gmail.com>
2024-02-29 16:07:44 +0400

    20240229160744

--- LIB/__SET_LIB.sh
+++ LIB/__SET_LIB.sh
@@ -235,8 +235,11 @@ function __SET_LOG () {
     fi
 
     #if "%__SET_LOG__%"=="1" (echo __SET_LOG__: %__SET_LOG__% && exit /b 0) else (set __SET_LOG__=1)
-
-    #if [[ "$__SET_LOG__" -eq 1 ]] ; then return 0 else __SET_LOG__=1 fi
+    if [[ "$__SET_LOG__" -eq 1 ]] ; then
+        return 0
+    else
+        __SET_LOG__=1
+    fi
 
     #------------------------------------------------------
     # LOG_DT_FORMAT -
