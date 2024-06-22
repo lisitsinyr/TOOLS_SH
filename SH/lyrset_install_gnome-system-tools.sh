61f159a0160d33c3a52bbc0696383624f63aef2e
lisitsinyr <lisitsinyr@gmail.com>
2024-06-22 14:01:23 +0400

    20240622140122

---
+++ SH/lyrset_install_gnome-system-tools.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+# -------------------------------------------------------------------
+# lyrset_install_gnome-system-tools.sh
+# -------------------------------------------------------------------
+
+#begin
+    # --------------------------------------------
+    # set_keyboard <Ctrl>Shift_R
+    # --------------------------------------------
+    #sudo apt-get update && sudo apt-get install gnome-system-tools
+    sudo apt update && sudo apt install gnome-system-tools
+
+#end
+
