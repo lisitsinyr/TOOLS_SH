f470ba7f6a0855f8c12f79ff5399364d390a1e55
lisitsinyr <lisitsinyr@gmail.com>
2024-06-25 11:43:40 +0400

    20240625114340

--- SH/lyrfd_CREATE_mnt_DATA.sh
+++ SH/lyrfd_CREATE_mnt_DATA.sh
@@ -9,10 +9,10 @@
     # ------------------------------------------------------------------
     if [[ ! -d "/mnt/DATA" ]] ; then
         sudo mkdir -p /mnt/DATA
-        cd /mnt/DATA
-        # Задаем права на созданный каталог
-        sudo chmod -R 770 /mnt/DATA
         # Задаем владельца на созданный каталог
         sudo chown -R lyr:lyr /mnt/DATA
+        # Задаем права на созданный каталог
+        sudo chmod -R 770 /mnt/DATA
     fi
+
 #end
